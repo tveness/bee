9e288bce77b490906aca3c67bfd3a14151b46d0a
Thomas Veness <veness@protonmail.com>
2024-08-15 10:39:34 +0100

    Use imports to make code more readable

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use miniz_oxide::inflate::decompress_to_vec;
+use postcard::from_bytes;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -8,9 +10,8 @@ pub struct WordMap(pub HashMap<String, Vec<String>>);
 
 pub fn load_sorted_words() -> Result<WordMap> {
     let sorted_words_bytes_compressed = include_bytes!("../sowpods_sorted.postcard.miniz");
-    let sorted_words_bytes =
-        miniz_oxide::inflate::decompress_to_vec(sorted_words_bytes_compressed).unwrap();
-    let sorted_words: WordMap = postcard::from_bytes(&sorted_words_bytes).unwrap();
+    let sorted_words_bytes = decompress_to_vec(sorted_words_bytes_compressed).unwrap();
+    let sorted_words: WordMap = from_bytes(&sorted_words_bytes).unwrap();
 
     Ok(sorted_words)
 }
